20d134a5024a89d16eb958936364a092d03fea77
Timothy Logan <timothy.b.logan@gmail.com>
2017-11-17 09:47:50 -0600

    test export script

---
+++ testExport.py
@@ -0,0 +1,6 @@
+from exporting.consolidate import consolidateGeometry
+from results.pts import RoomGrid
+
+#consolidateGeometry(r"\\nt11\00\00603.000\07_Software\Radiance\cloudWorkflow\09072017Mez\simulation", r"D:\Projects\AZURE\simTest001")
+y = RoomGrid(r"\\nt11\00\00603.000\07_Software\Radiance\cloudWorkflow\09072017Mez\simulation\unnamed\gridBasedSimulation\points.txt")
+print y
